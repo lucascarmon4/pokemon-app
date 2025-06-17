254d083b9a4f5da2e30891990fde50ea8d597329
lucascarmon4 <lucascarmonaneto510@gmail.com>
2025-06-17 12:04:08 -0300

    adding new interfaces for pokemon details

--- src/types/Pokemon.ts
+++ src/types/Pokemon.ts
@@ -8,3 +8,32 @@ export interface PokemonListResponse {
     next: string | null;
     previous: string | null;
 }
+
+export interface Stat {
+    base_stat: number;
+    stat: { name: string };
+}
+
+export interface Ability {
+    ability: { name: string };
+}
+
+export interface Type {
+    type: { name: string };
+}
+
+export interface PokemonDetails {
+    name: string;
+    sprites: {
+        other: {
+            ["official-artwork"]: {
+                front_default: string;
+            };
+        };
+    };
+    height: number;
+    weight: number;
+    abilities: Ability[];
+    stats: Stat[];
+    types: Type[];
+}
