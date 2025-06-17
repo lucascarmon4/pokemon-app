dc9873dc99ee49f6a37df98a1484ffc9ec7131d9
lucascarmon4 <lucascarmonaneto510@gmail.com>
2025-06-17 12:04:32 -0300

    adding new route for pokemon details

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-// import Details from "./pages/Details";
+import Details from "./pages/Details";
 
 export default function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                {/* <Route path="/pokemon/:name" element={<Details />} /> */}
+                <Route path="/pokemon/:name" element={<Details />} />
             </Routes>
         </Router>
     );
